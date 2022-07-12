a25d2f849700b5e03b6ecafd7aa4566561cd7d3d
JacobHollister <48007098+JacobHollister@users.noreply.github.com>
2022-07-13 00:28:31 +1000

    removed unnessacary code

--- server/app.js
+++ server/app.js
@@ -16,9 +16,6 @@ const app = express();
 
 app.use(helmet())
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('common'));
 app.use(cors())
@@ -32,22 +29,6 @@ app.use('/user', usersRouter);
 app.use('/api/countries', countriesRouter);
 app.use('/api/volcanoes', volcanoesRouter);
 app.use('/api/volcano', volcanoRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 const port = process.env.PORT || 5000;
 
